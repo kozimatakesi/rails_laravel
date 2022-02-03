071a5da840dc507019dfa489c580c5228e211ab0
riverbook708 <riverbook708@gmail.com>
2022-02-03 23:36:47 +0900

    change routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+
+// Route::post('/login/guest',
+//     'Auth\LoginController@guestLogin')->name('guest_login');
 
 Route::get('/{any}', function() {
     return view('layouts.app');
